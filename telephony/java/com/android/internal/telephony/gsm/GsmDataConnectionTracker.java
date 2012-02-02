27f786370b91c7462a2e884cff08ec146f6d6cea
Ajay Nambi <anambi@codeaurora.org>
2012-02-01 16:55:15 -0800

    Telephony: Optimizations on alarms used
    * Use non wakeup alarms for data stall detection
    * Stop data stall alarms during dormancy
    
    CRs-Fixed: 334349
    (cherry picked from commit 3512da1c22a8bdb82d633b93a6f2de608f1b4b08)
    
    Change-Id: I2a7654d7cf888d63bf3862d33f126218c409ffd4

--- telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
@@ -1365,6 +1365,7 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
                 log("onDataStateChanged: Data Activity updated to DORMANT. stopNetStatePoll");
             }
             stopNetStatPoll();
+            stopDataStallAlarm();
         } else {
             mActivity = Activity.NONE;
             if (DBG) {
@@ -1372,7 +1373,10 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
                          "isAnyDataCallActive = " + isAnyDataCallActive +
                          " isAnyDataCallDormant = " + isAnyDataCallDormant);
             }
-            if (isAnyDataCallActive) startNetStatPoll();
+            if (isAnyDataCallActive) {
+                startNetStatPoll();
+                startDataStallAlarm(DATA_STALL_NOT_SUSPECTED);
+            }
         }
         mPhone.notifyDataActivity();
 
@@ -1795,7 +1799,7 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
         intent.putExtra(DATA_STALL_ALARM_TAG_EXTRA, mDataStallAlarmTag);
         mDataStallAlarmIntent = PendingIntent.getBroadcast(mPhone.getContext(), 0, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
-        am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+        am.set(AlarmManager.ELAPSED_REALTIME,
                 SystemClock.elapsedRealtime() + delayInMs, mDataStallAlarmIntent);
     }
 
